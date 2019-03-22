f7b00fc6a876beb1cac892dae4d4283bf7862011
maupeon <maupeon@gmail.com>
2019-03-22 17:50:34 -0600

    sudo

--- db/sharding.js
+++ db/sharding.js
@@ -1,19 +1,19 @@
 # Crear una red para el Sharding
 
-docker network create mongo-sh
+sudo docker network create mongo-sh
 # -----------------------------------------------
 # ------ Configurar el Config Replica Set -------
 # -----------------------------------------------
 
 # Crear tres contenedores para los nodos del Config Replica Set
 
-docker run --name mongo-config1 -d --net mongo-sh mongo --replSet "rsConfig" --configsvr
-docker run --name mongo-config2 -d --net mongo-sh mongo --replSet "rsConfig" --configsvr
-docker run --name mongo-config3 -d --net mongo-sh mongo --replSet "rsConfig" --configsvr
+sudo docker run --name mongo-config1 -d --net mongo-sh mongo --replSet "rsConfig" --configsvr
+sudo docker run --name mongo-config2 -d --net mongo-sh mongo --replSet "rsConfig" --configsvr
+sudo docker run --name mongo-config3 -d --net mongo-sh mongo --replSet "rsConfig" --configsvr
 
 # Iniciar una terminal en uno de los nodos
 
-docker exec -it mongo-config1 bash
+sudo docker exec -it mongo-config1 bash
 
 # Conectarse a uno de los nodos 
 
@@ -53,13 +53,13 @@ exit  # Para salir del contenedor
 
 # Crear tres contenedores para los nodos del Shard Replica Set
 
-docker run --name mongo-shard11 -d --net mongo-sh mongo --replSet "rsShard1" --shardsvr
-docker run --name mongo-shard12 -d --net mongo-sh mongo --replSet "rsShard1" --shardsvr
-docker run --name mongo-shard13 -d --net mongo-sh mongo --replSet "rsShard1" --shardsvr
+sudo docker run --name mongo-shard11 -d --net mongo-sh mongo --replSet "rsShard1" --shardsvr
+sudo docker run --name mongo-shard12 -d --net mongo-sh mongo --replSet "rsShard1" --shardsvr
+sudo docker run --name mongo-shard13 -d --net mongo-sh mongo --replSet "rsShard1" --shardsvr
 
 # Iniciar una terminal en uno de los nodos
 
-docker exec -it mongo-shard11 bash
+sudo docker exec -it mongo-shard11 bash
 
 # Conectarse a uno de los nodos 
 
@@ -96,5 +96,5 @@ exit  # Para salir del contenedor
 # ------ Iniciar el Router -------
 # --------------------------------
 
-docker run  --name mongo-router -d --net mongo-sh mongo  mongos --configdb rsConfig/mongo-config1:27019,mongo-config2:27019,mongo-config3:27019
+sudo docker run  --name mongo-router -d --net mongo-sh mongo  mongos --configdb rsConfig/mongo-config1:27019,mongo-config2:27019,mongo-config3:27019
 
